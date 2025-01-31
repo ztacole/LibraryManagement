af838c465b0fd8d4a68f916cd48ea93d92f4d6f5
Jeki <ahmadzaqi490@gmail.com>
2025-01-31 08:39:52 +0700

    Lengkapin model

---
+++ src/model/Peminjaman.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author ahmad
+ */
+public class Peminjaman {
+    private int id, idAnggota, idPetugas, denda;
+    private Date tanggalPeminjaman, tanggalPengembalian;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdAnggota() {
+        return idAnggota;
+    }
+
+    public void setIdAnggota(int idAnggota) {
+        this.idAnggota = idAnggota;
+    }
+
+    public int getIdPetugas() {
+        return idPetugas;
+    }
+
+    public void setIdPetugas(int idPetugas) {
+        this.idPetugas = idPetugas;
+    }
+
+    public int getDenda() {
+        return denda;
+    }
+
+    public void setDenda(int denda) {
+        this.denda = denda;
+    }
+
+    public Date getTanggalPeminjaman() {
+        return tanggalPeminjaman;
+    }
+
+    public void setTanggalPeminjaman(Date tanggalPeminjaman) {
+        this.tanggalPeminjaman = tanggalPeminjaman;
+    }
+
+    public Date getTanggalPengembalian() {
+        return tanggalPengembalian;
+    }
+
+    public void setTanggalPengembalian(Date tanggalPengembalian) {
+        this.tanggalPengembalian = tanggalPengembalian;
+    }
+}
