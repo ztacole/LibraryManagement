60f4ac7fe71c4f362ceffda9d11a4be03287c913
Jeki <ahmadzaqi490@gmail.com>
2025-02-07 08:15:40 +0700

    dao

--- src/data/dao/DetailPeminjamanDAO.java
+++ src/data/dao/DetailPeminjamanDAO.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 import koneksi.Koneksi;
 import model.DetailPeminjaman;
 
@@ -26,22 +27,40 @@ public class DetailPeminjamanDAO {
         conn = Koneksi.getKoneksi();
     }
     
-    public ArrayList<DetailPeminjaman> getDetailPeminjamanById(int idPeminjaman) {
-        ArrayList<DetailPeminjaman> listDetailPeminjaman = new ArrayList<>();
-        String query = "SELECT * from detail_peminjaman where id_peminjaman = ?";
-        
+    public ArrayList<DetailPeminjaman> getListDetailPeminjamanById(int idPeminjaman) {
+        ArrayList<DetailPeminjaman> listDetail = new ArrayList<>();
+        String query = "SELECT b.judul, dp.jumlah " +
+                       "FROM detail_peminjaman dp " +
+                       "JOIN buku b ON dp.id_buku = b.id " +
+                       "WHERE dp.id_peminjaman = ?";
+
         try (PreparedStatement ps = conn.prepareStatement(query)) {
             ps.setInt(1, idPeminjaman);
-            
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
-                    DetailPeminjaman detailPeminjaman = new DetailPeminjaman();
+                    DetailPeminjaman detail = new DetailPeminjaman();
+                    detail.setJudul(rs.getString("judul"));
+                    detail.setJumlah(rs.getInt("jumlah"));
+                    listDetail.add(detail);
                 }
             }
         } catch (SQLException ex) {
             Logger.getLogger(DetailPeminjamanDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        return listDetailPeminjaman;
+
+        return listDetail;
+    }
+
+    public DefaultTableModel getDetailPeminjamanModel(int idPeminjaman) {
+        ArrayList<DetailPeminjaman> listDetail = getListDetailPeminjamanById(idPeminjaman);
+        Object[][] dataTabel = new Object[listDetail.size()][2];
+
+        for (int i = 0; i < listDetail.size(); i++) {
+            dataTabel[i][0] = listDetail.get(i).getJudul();
+            dataTabel[i][1] = listDetail.get(i).getJumlah();
+        }
+
+        String[] columnNames = {"Judul Buku", "Jumlah"};
+        return new DefaultTableModel(dataTabel, columnNames);
     }
 }
